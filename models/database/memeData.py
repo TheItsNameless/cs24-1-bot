0d6adb5aa8c47d5a2761d288b9ba7e9eac0d3be8
TheItsNamless <itsnameless@outlook.com>
2024-10-16 14:04:13 +0200

    add content to meme table

--- models/database/memeData.py
+++ models/database/memeData.py
@@ -13,11 +13,12 @@ class MemeFormat(Enum):
 
 
 class Meme(BaseModel):
-    uuid = fields.UUIDField(pk=True)
-    format = fields.CharEnumField(MemeFormat, max_length=3)
-    message = fields.TextField()
+    uuid = fields.UUIDField(pk=True, description="The UUID of the meme image")
+    format = fields.CharEnumField(MemeFormat, max_length=3, description="The format of the meme image")
+    message = fields.TextField(description="The message that the user attached to the meme image")
+    content = fields.TextField(description="The OCRed content of the meme image")
     author: fields.ForeignKeyRelation[User] = fields.ForeignKeyField("models.User", related_name="memes")
-    date = fields.DatetimeField()
+    date = fields.DatetimeField(description="The date the meme was sent")
 
     def __str__(self):
         return self.uuid
--- models/database/userData.py
+++ models/database/userData.py
@@ -4,9 +4,12 @@ from models.database.baseModel import BaseModel
 
 
 class User(BaseModel):
-    id = fields.IntField(pk=True)
-    global_name = fields.CharField(max_length=255)
-    display_name = fields.CharField(max_length=255)  # Maybe outdated because it is not updated on every message
+    id = fields.IntField(pk=True, description="The ID of the user on Discord")
+    global_name = fields.CharField(max_length=255, description="The name of the user on Discord")
+    display_name = fields.CharField(
+        max_length=255,
+        description="The display name of the user on the Server")  # Maybe outdated because it is not updated on
+    # every message
 
     def __str__(self):
         return self.display_name
